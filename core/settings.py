8520b25515a69f512621cb81a60faed7d9ec2371
SRB-95 <bsaumyaranjan1995@gmail.com>
2021-04-22 11:57:51 +0530

    rankltRightMedia

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+import django_heroku
 
 from pathlib import Path
 
@@ -121,5 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Active Django-Heroku.
+django_heroku.settings(locals())
